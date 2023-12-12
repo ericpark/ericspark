a443fbd7673b8aa331dd0eb7523ba28eec16efbe
ericpark <4909815+ericpark@users.noreply.github.com>
2023-12-12 15:16:22 -0500

    created tech page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,39 +1,46 @@
-import React from "react"
-import { graphql, StaticQuery } from "gatsby"
+import React from "react";
+import { graphql, StaticQuery } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-import "../utils/normalize.css"
-import "../utils/css/screen.css"
+import "../utils/normalize.css";
+import "../utils/css/screen.css";
 
 const AboutPage = ({ data }, location) => {
-  const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title;
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO
-        title="About"
-        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-      />
+    <Layout location={"about"} title={siteTitle}>
+      <SEO title="About" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
 
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h2 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-                Eric the _______  
+            Eric the _______
           </h2>
-          <div style={{ textAlign: 'center', margin: '2 2 2 2', padding: '2 2 2 2'}}>            
+          <div
+            style={{
+              textAlign: "center",
+              margin: "2 2 2 2",
+              padding: "2 2 2 2"
+            }}
+          >
             <p>
-                  entrepreneur. dry cleaner. climber. dog trainer. campaign staff member. implementation consultant. system architect. flutter developer. 
-                  solutions consultant. front end developer. caterer. iOS developer. IT consultant. head volunteer.
-                  fried chicken restaurant server. G51 venture scholar. md circuit court intern.  bbq restaurant server. 
-                  published medical journal author. smoothie maker. backend engineer. neurolab volunteer. food service manager.  
-                  alumni call center representative. pizza tosser/almost-dropper. volunteer hospital ambassador. 
-                  how i met your mother enthusiast. ex-bostonian. traveler. cto. 
+              entrepreneur. dry cleaner. climber. dog trainer. campaign staff
+              member. implementation consultant. system architect. flutter
+              developer. solutions consultant. front end developer. caterer. iOS
+              developer. IT consultant. head volunteer. fried chicken restaurant
+              server. G51 venture scholar. md circuit court intern. bbq
+              restaurant server. published medical journal author. smoothie
+              maker. backend engineer. neurolab volunteer. food service manager.
+              alumni call center representative. pizza tosser/almost-dropper.
+              volunteer hospital ambassador. how i met your mother enthusiast.
+              ex-bostonian. traveler. cto.
             </p>
             <p>let's add some more.</p>
           </div>
-         {/*} <figure className="kg-card kg-image-card kg-width-full">
+          {/*} <figure className="kg-card kg-image-card kg-width-full">
             <Img
               fluid={data.benchAccounting.childImageSharp.fluid}
               className="kg-image"
@@ -54,8 +61,8 @@ const AboutPage = ({ data }, location) => {
         </div>
       </article>
     </Layout>
-  )
-}
+  );
+};
 
 const indexQuery = graphql`
   query {
@@ -64,7 +71,9 @@ const indexQuery = graphql`
         title
       }
     }
-    benchAccounting: file(relativePath: { eq: "bench-accounting-49909-unsplash.jpg" }) {
+    benchAccounting: file(
+      relativePath: { eq: "bench-accounting-49909-unsplash.jpg" }
+    ) {
       childImageSharp {
         fluid(maxWidth: 1360) {
           ...GatsbyImageSharpFluid
@@ -72,7 +81,7 @@ const indexQuery = graphql`
       }
     }
   }
-`
+`;
 
 export default props => (
   <StaticQuery
@@ -81,4 +90,4 @@ export default props => (
       <AboutPage location={props.location} data={data} {...props} />
     )}
   />
-)
+);
